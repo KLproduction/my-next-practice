d1ea82275f3ffb56af5c4707e35df1b405000e48
KLproduction <kent.law.production01@gmail.com>
2024-08-15 10:56:43 +0100

    commit

--- app/@admin/page.jsx
+++ app/@admin/page.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const AdminPage = () => {
   return (
     <div>
-
-        <div>AdminPage</div>
-        <p>It is admin page</p>
-        <div className=' border-red-600 border w-[500px] h-[500px]  bg-zinc-800'></div>
+      <div>AdminPage</div>
+      <p>This is Admin page</p>
+      <div className=" border-red-600 border w-[500px] h-[500px]  bg-zinc-800"></div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
